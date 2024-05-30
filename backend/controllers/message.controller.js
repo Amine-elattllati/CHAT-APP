2cd4ed12d63b0b9a85aed409588250e2d08bf332
Amine-elattllati <126120495+Amine-elattllati@users.noreply.github.com>
2024-05-30 13:21:17 +0100

    Get Messages Endpoint and Get Users for Sidebar Endpoint

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -23,8 +23,38 @@ export const sendMessage = async (req, res) =>{
         if(newMessage){
             conversation.messages.push(newMessage._id);
         }
+
+        // SOCKET IO FUNCTIONALITY WILL GO HERE
+
+
+       // await conversation.save();
+        //await newMessage.save();
+
+        //this will run in parallel
+        await Promise.all([conversation.save(), newMessage.save()]);
     } catch (error) {
         console.log("Error in message controller: ", error.message)
         res.status(500).json({ error:"Internal server error"});
     }
 };
+
+export const getMessages = async (req, res) => {
+
+    try {
+        const {id:userToChatId}= req.params;
+        const senderId = req.uder._id;
+
+        const conversation = await Conversation.findOne({
+            participants: {$all: [senderId, userToChatId]},
+        }).populate("messages");  // NOT REFERENCE BUT ACTUAL MESSAGES
+
+        if(!conversation) return res.status(200).json([]);
+
+        const messages = conversation.messages;
+
+        res.status(200).json(messages);
+    } catch (error) {
+        console.log("Error in message controller: ", error.message)
+        res.status(500).json({ error:"Internal server error"});
+    }
+}
